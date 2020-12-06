468670cb90fdd6851dce60efea6088aded6a4d3c
Basov Andrey <andreybasov0@gmail.com>
2020-12-07 00:22:04 +0300

    Updates

---
+++ precache-manifest.759ffa5ce761895f40731ebd770bf0e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d568678f51b2f8e46f67c686a9cdba9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e8b581c4a9dad1a1c24",
+    "url": "/static/js/2.0fc0252b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0fc0252b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad76b8d41e3c02262017",
+    "url": "/static/js/main.77cb2109.chunk.js"
+  },
+  {
+    "revision": "0939b0f714c2b1ff39ed",
+    "url": "/static/js/runtime-main.6167e78a.js"
+  }
+]);
